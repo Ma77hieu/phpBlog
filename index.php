1ef5993afd3f0f5399a80dab67b3eb61de390dfc
matthieu-ortlieb <matthieu.ortlieb@cafefrappe.fr>
2023-09-03 16:46:30 +0200

    Initiating front, blogpost list page structure OK (need some visual improvements)

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+error_reporting(E_ERROR);
 
 //loading vendor
 require_once realpath(__DIR__ . '/vendor/autoload.php');
