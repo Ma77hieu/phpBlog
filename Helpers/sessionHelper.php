41edc7b08481f046ea5c0a96800b4a46848be0de
matthieu-ortlieb <matthieu.ortlieb@cafefrappe.fr>
2023-09-25 18:40:27 +0200

    linting

--- Helpers/sessionHelper.php
+++ Helpers/sessionHelper.php
@@ -1,6 +1,13 @@
 <?php
 
-function manageCsrf(){
+/**
+ * Manage the csrf token creation
+ *
+ * @throws Exception
+ * @return void
+ */
+function manageCsrf()
+{
     $_SESSION['csrfToken'] = bin2hex(random_bytes(35));
-    echo("<div style=\"display: none;\">".$_SESSION['csrftoken']."</div>");
+    echo("<div style=\"display: none;\">" . $_SESSION['csrftoken'] . "</div>");
 }
