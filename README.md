aabe3a90b2a5fe4a36fc377f27ce9415286c212d
matthieu-ortlieb <matthieu.ortlieb@cafefrappe.fr>
2023-09-25 18:45:18 +0200

    Updated readme with corresponding database file

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
-# phpBlog
+# How to install myPhpBlog project
 
-add a .env to your root folder by copy/paste the .env.example file and populate it with your own environment (database name, user and password for example)
+### 1.Clone the repository
+Open a command prompt and open the directory where you want to install the project
+run `git clone https://github.com/Ma77hieu/phpBlog.git`
+
+### 2.Install required libraries
+run `composer install`
+
+### 3.Create and setup the .env file
+take the .env.example file and rename it ".env", replace all data inside this file with your own actual environment data.
+If you have doubts regarding all SMTP variables, please refer to your email provider documentation.
+
+### 4.Create your database
+following instructions applies for mysql: while being in the root directory run `mysql -u username -p database_name < SQL/blog.sql`
+
+### 5.Discover the blog
+The database already contains some example data so you can discover the site not completely empty of content.
--- SQL/blog.sql
+++ SQL/blog.sql
@@ -1,41 +1,167 @@
-CREATE DATABASE IF NOT EXISTS blog;
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:8889
+-- Généré le : lun. 25 sep. 2023 à 16:30
+-- Version du serveur : 5.7.39
+-- Version de PHP : 7.4.33
 
-USE blog;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-create table IF NOT EXISTS user
-(
-    user_id int auto_increment
-        primary key,
-    email varchar(45) not null,
-    password varchar(255) not null,
-    roles varchar(255) not null
-);
 
-create table IF NOT EXISTS blogpost
-(
-    blogpost_id int auto_increment
-        primary key,
-    title tinytext not null,
-    summary tinytext not null,
-    content text null,
-    author int not null,
-    creation_date datetime not null,
-    modification_date datetime null,
-    constraint blogpost_user_user_id_fk
-        foreign key (author) references user (user_id)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-create table IF NOT EXISTS comment
-(
-    comment_id int auto_increment,
-    title tinytext not null,
-    text text not null,
-    author int null,
-    creation_date datetime not null,
-    modification_date datetime null,
-    constraint comment_comment_id_uindex
-        unique (comment_id),
-    constraint comment_user_user_id_fk
-        foreign key (author) references user (user_id)
-);
+--
+-- Base de données : `blog`
+--
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `blogpost`
+--
+
+CREATE TABLE `blogpost` (
+  `blogpost_id` int(11) NOT NULL,
+  `title` tinytext NOT NULL,
+  `summary` tinytext NOT NULL,
+  `content` text,
+  `author` int(11) NOT NULL,
+  `creation_date` datetime NOT NULL,
+  `modification_date` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `blogpost`
+--
+
+INSERT INTO `blogpost` (`blogpost_id`, `title`, `summary`, `content`, `author`, `creation_date`, `modification_date`) VALUES
+(13, 'Les erreurs que tous les developpers junior font', 'Retrouvez les 3 erreurs les plus courantes chez les devs junior', '1- Avoir peur de demander de l’aide\r\n2- Ne pas dire quand on ne sait pas\r\n3- Le syndrome de l’imposteur', 33, '2023-09-24 08:13:59', '2023-09-24 14:48:53'),
+(19, 'Les outils indispensables pour dev', 'La boîte à outils de nos développeurs aux quotidiens', 'Les développeurs sont constamment à la recherche d’outils qui peuvent améliorer leur efficacité et productivité au quotidien. Les logiciels de développement ont évolué ces dernières années pour offrir un large éventail de fonctionnalités et de capacités.', 33, '2023-09-22 13:05:11', NULL),
+(20, 'Langages de programmation les mieux payés en 2023', 'Palmarès 2023 des langages amenant aux meilleurs salaires', 'Si vous voulez vous lancer dans la programmation web ou si vous désirez vous spécialiser dans un langage particulier, cela peut être une bonne idée de chercher à savoir quels sont les langages de programmation les mieux payés du moment.\r\n\r\nEn effet, le développement web est non seulement l’un des secteurs du numérique qui recrute le plus ces dernières années en France, mais il fait surtout partie de ceux qui rémunèrent le mieux.\r\n\r\nDe nouvelles technologies voient régulièrement le jour et les entreprises n’hésitent pas à mettre le prix qu’il faut pour dénicher les meilleurs talents.', 33, '2023-09-21 16:37:13', NULL),
+(21, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.', 33, '2023-09-20 16:11:04', NULL),
+(22, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.', 33, '2023-09-19 09:32:17', NULL),
+(23, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.', 33, '2023-09-19 09:32:17', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `comment`
+--
+
+CREATE TABLE `comment` (
+  `comment_id` int(11) NOT NULL,
+  `title` tinytext NOT NULL,
+  `text` text NOT NULL,
+  `author` int(11) DEFAULT NULL,
+  `blogpost` int(11) NOT NULL,
+  `creation_date` datetime NOT NULL,
+  `modification_date` datetime DEFAULT NULL,
+  `is_validated` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `comment`
+--
+
+INSERT INTO `comment` (`comment_id`, `title`, `text`, `author`, `blogpost`, `creation_date`, `modification_date`, `is_validated`) VALUES
+(16, 'Très intéressant', 'Merci pour ce blogpost de qualité', 33, 13, '2023-09-25 16:25:40', NULL, 1),
+(17, 'Bof...', 'Ce blogpost manque un peu de substance', 33, 13, '2023-09-25 16:26:11', NULL, 1),
+(18, 'Je m\'y reconnais', 'C\'est effectivement tout à fait ce que j\'ai vécu', 1, 13, '2023-09-25 16:27:24', NULL, 1),
+(19, 'des supers produits', 'Venez achetr me sproduits incroyables d\'hygiène sur superProds.com', 33, 13, '2023-09-25 16:29:10', NULL, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `roles` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`user_id`, `email`, `password`, `roles`) VALUES
+(1, 'user@user.com', 'ab56b4d92b40713acc5af89985d4b786', 'user'),
+(33, 'admin@admin.com', 'ab56b4d92b40713acc5af89985d4b786', 'user,admin');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `blogpost`
+--
+ALTER TABLE `blogpost`
+  ADD PRIMARY KEY (`blogpost_id`),
+  ADD KEY `blogpost_user_user_id_fk` (`author`);
+
+--
+-- Index pour la table `comment`
+--
+ALTER TABLE `comment`
+  ADD UNIQUE KEY `comment_comment_id_uindex` (`comment_id`),
+  ADD KEY `comment_user_user_id_fk` (`author`),
+  ADD KEY `comment_blogpost_blogpost_id_fk` (`blogpost`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `blogpost`
+--
+ALTER TABLE `blogpost`
+  MODIFY `blogpost_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT pour la table `comment`
+--
+ALTER TABLE `comment`
+  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `blogpost`
+--
+ALTER TABLE `blogpost`
+  ADD CONSTRAINT `blogpost_user_user_id_fk` FOREIGN KEY (`author`) REFERENCES `user` (`user_id`);
+
+--
+-- Contraintes pour la table `comment`
+--
+ALTER TABLE `comment`
+  ADD CONSTRAINT `comment_blogpost_blogpost_id_fk` FOREIGN KEY (`blogpost`) REFERENCES `blogpost` (`blogpost_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `comment_user_user_id_fk` FOREIGN KEY (`author`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
